cd2027cd2f47dbd83dc7b9c8664aff218d4a3cc1
thePi314 <the.pi@outlook.com>
2020-03-29 18:23:57 +0200

    Toggler For Component Settings

--- assets/js/components/application.js
+++ assets/js/components/application.js
@@ -1,6 +1,45 @@
+class PrepareHeader {
+    constructor() {
+        this.target         = document.querySelector('.header');
+        this.toolbar        = null;
+        this.header_buttons = null;
+
+        this.prepareToolbar();
+        this.prepareButtons();
+    }
+
+    createButton(icon,id=null) {
+        let button = document.createElement('a');
+        button.classList.add('button');
+        button.innerHTML = '<span class="material-icons">'+icon+'</span>';
+        if (id != null) 
+            button.id = id;
+        
+        return button;
+    }
+
+    prepareButtons() {
+        this.header_buttons = document.createElement('div');
+        this.header_buttons.classList.add('header-buttons');
+
+        this.header_buttons.append(this.createButton('exit_to_app'));
+        this.header_buttons.append(this.createButton('menu','component-settings-toggler'));
+        
+        this.target.append(this.header_buttons);
+    }
+
+    prepareToolbar() {
+        this.toolbar = document.createElement('div');
+        this.toolbar.classList.add('header-toolbar');
+
+        this.target.append(this.toolbar);
+    }
+}
+
 class ComponentApplication extends SPAComponent {
     constructor() {
         super('application','/components/application');
+        this.header = null;
     }
 
     load_events(spa_controller = null) {
@@ -9,22 +48,26 @@ class ComponentApplication extends SPAComponent {
         this.prepareHeader();
     }
 
+    load_toggler_events() {
+        let togglers = document.querySelectorAll('#component-settings-toggler');
+        togglers.forEach( toggler => {
+            toggler.addEventListener('mousedown',() => {
+                let sidebar = document.querySelector('.component-settings');
+                if( sidebar.classList.contains('collapsed') )
+                    sidebar.classList.remove('collapsed');
+                else
+                sidebar.classList.add('collapsed');
+            })
+        })
+    }
+
     hideElements() {
         document.querySelector('body > wl-image-slider').classList.add('hide');
         document.querySelector('body > #scroll-target').classList.add('hide');
     }
 
     prepareHeader() {
-        let header = document.querySelector('.header');
-
-        let header_toolbar = document.createElement('div');
-        header_toolbar.classList.add('header-toolbar'); 
-        
-
-        let header_buttons = document.createElement('div');
-        header_buttons.classList.add('header-buttons');
-
-        header.append(header_toolbar);
-        header.append(header_buttons);
+        this.header = new PrepareHeader();
+        this.load_toggler_events();
     }
 }
